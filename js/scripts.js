5087c1d0e6b1b3a90bb74c03fc941592ca72c283
Ben Spenard <bdspen@gmail.com>
2015-09-11 12:05:16 -0700

    add jQuery in scripts.js, app works correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,45 @@
 var replace = function(str, toReplace, newWord){
     return str.replace(toReplace, newWord)
 };
+
+$(document).ready(function() {
+    $("form#str").submit(function(event){
+        var str = $("input#str").val();
+        var toReplace = $("input#toreplace").val();
+        var newWord = $("input#newWord").val();
+        var result = replace(str, toReplace, newWord);
+        $(".string").text(result);
+        $("#result").show();
+        event.preventDefault();
+    });
+});
+
+//**Trying to make the sentence words populate a html selector**
+
+// function setOption(str)
+// {
+//     var strArr = str.split(" ");
+//     var selector = document.getElementById("selectword");
+//     for(var i = 0; i < strArr.length; i++){
+//         var option = document.createElement("option");
+//         option.innerHTML = strArr[i];
+//         option.value = strArr[i];
+//         sel.appendChild(option);
+//     }
+// }
+
+// $(document).ready(function() {
+//     $("form#str").submit(function(event){
+//         var str = $("form#string").val();
+//         var strArr = str.split(" ");
+//         var selector = document.getElementById("selectword");
+//         for(var i = 0; i < strArr.length; i++){
+//             var option = document.createElement("option");
+//             option.innerHTML = strArr[i];
+//             option.value = strArr[i];
+//             sel.appendChild(option);
+//         }
+//
+//         event.preventDefault();
+//     });
+// });
